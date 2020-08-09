cc40b20e805c6cbfde51335c071020614004ff4c
MaxwelSantana <maxwelsant96@icloud.com>
2020-08-09 14:44:49 -0300

    finished apis calls

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,32 +3,42 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/17615307?s=460&u=c3e626322df7be24c50ce71ce9ae3b9876da29e2&v=4"
-                    alt="Maxwel Santana" />
+                <img src={teacher.avatar}
+                    alt={teacher.name} />
                 <div>
-                    <strong>Maxwel Santana</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Entusiasta das melhores tecnologias de química avançada
-                        <br /><br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências
-                    </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                            <strong>R$ 80,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                             Entrar em contato
-                        </button>
+                </a>
             </footer>
         </article>
     );
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
@@ -10,6 +11,8 @@ import Select from '../../components/Select';
 import api from '../../services/api';
 
 function TeacherForm() {
+    const history = useHistory();
+
     const [name, setName] = useState('');
     const [avatar, setAvatar] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
@@ -53,6 +56,7 @@ function TeacherForm() {
             schedule: scheduleItems
         }).then(() => {
             alert('Cadastro realizado com sucesso');
+            history.push('/');
         }).catch(() => {
             alert('Erro no cadastro');
         })
